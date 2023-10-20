e2324e7369a14f24f61dd02b7de4fcc3b6bcdb14
Abhishek T R <130368708+abhiabhishektr@users.noreply.github.com>
2023-10-20 09:55:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let a=document.getElementById("ale");
+a.addEventListener("click",alerting)
+function alerting(){
+    alert("connection not extablished")
+}
+
+let c=document.getElementById("contact");
+c.addEventListener("click",alerting)
+function alerting(){
+    alert("contact not possible right now")
+}
+
+let b=document.getElementById("explore");
+b.addEventListener("click",alerting)
+function alerting(){
+    alert("Explore done ")
+}
+
+
